c46a29db5f25d65fb11fe1f79d4de1c0a6fff57b
chasechristensen1 <chase.christensen@inmoment.com>
2025-05-07 13:39:09 -0600

    Update site URL in astro.config.mjs to point to the production environment

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://astro-static-production.up.railway.app",
 
   integrations: [mdx(), sitemap(), showTailwindcssBreakpoint()],
 
