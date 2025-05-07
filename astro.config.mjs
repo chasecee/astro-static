faf642fd9960e1cc5816b90ef09787204054fb5f
Chase Christensen <chaserchristensen@gmail.com>
2025-05-07 10:43:43 -0600

    Add Tailwind CSS support and integrate new plugins; update global styles and components for improved design. Enhance Header and BaseHead components with dynamic font loading and active link management.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,28 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig, fontProviders } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import swup from "@swup/astro";
+import showTailwindcssBreakpoint from "astro-show-tailwindcss-breakpoint";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+
+  integrations: [mdx(), sitemap(), showTailwindcssBreakpoint(), swup()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Poppins",
+        cssVariable: "--font-poppins",
+      },
+    ],
+  },
 });
